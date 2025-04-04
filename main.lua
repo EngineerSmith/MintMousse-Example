de206ae523786b4eb0cf14f508653e40ba5adb22
EngineerSmith <56016593+EngineerSmith@users.noreply.github.com>
2025-04-04 19:37:11 +0100

    Added MM.quit

--- main.lua
+++ main.lua
@@ -11,15 +11,17 @@ love.mintmousse.start({
   whitelist = { "127.0.0.1", "192.168.0.0/16" }
 })
 
-local cardTextID = "example"
-
 local tab = love.mintmousse.newTab("Dashboard", "dashboard")
-tab:newCard({ size = 5, isContentCenter = true })
+local card = tab:newCard({
+    size = 5,
+    isContentCenter = true,
+    borderColor = "error",
+  })
   :addCardHeader({ text = "header", isTransparent = true })
   :newCardBody()
     :addCardTitle({ text = "Title text" })
     :addCardSubtitle({ text = "Subtitle text" })
-    :addCardText({ id = cardTextID, text = "World" })
+    :addCardText({ id = "example", text = "World" })
     :addProgressBar({
         id = "ProgressBar1",
         percentage = 0,
@@ -27,9 +29,13 @@ tab:newCard({ size = 5, isContentCenter = true })
         isStriped = false,
         color = "danger",
       })
+    .back
+  :addCardFooter({ id = "footer", text = "footer" })
 
-local cardText = love.mintmousse.get(cardTextID)
-local progressBar = love.mintmousse.get("ProgressBar1")
+--local cardText = love.mintmousse.get("example")
+--local progressBar = love.mintmousse.get("ProgressBar1")
+
+local footer = love.mintmousse.get("footer")
 
 local timer, flop = 0, false
 local lastInterval = -1
@@ -38,26 +44,33 @@ love.update = function(dt)
   local currentInterval = math.floor(timer / 0.1)
   if currentInterval > lastInterval then
     lastInterval = currentInterval
-    progressBar.percentage = math.max(math.min((timer / 4.8) * 100, 100), 0)
+    --progressBar.percentage = math.max(math.min((timer / 2.8) * 100, 100), 0)
   end
-  while timer >= 5 do
-    timer = timer - 5
+  while timer >= 3 do
+    timer = timer - 3
     lastInterval = -1
     flop = not flop
     if flop then
-      cardText.text = "Hello"
-      progressBar.isStriped = not progressBar.isStriped
-      progressBar.color = "danger"
+      --cardText.text = "Hello"
+      --progressBar.isStriped = not progressBar.isStriped
+      --progressBar.color = "danger"
+      card.borderColor = "info"
+      footer.text = "hello"
+      --love.mintmousse.notify({ title = "MintMousse" })
     else
-      cardText.text = "World"
-      progressBar.color = "warning"
+      --cardText.text = "World"
+      --progressBar.color = "warning"
+      card.borderColor = "success"
+      footer.text = nil
+      --love.mintmousse.notify({ title = "MintMousse" })
     end
   end
 end
 
 love.mousepressed = function(_, _, button)
   if button == 1 then
-    cardText.text = "Mouse button was pressed!"
+    --cardText.text = "Mouse button was pressed!"
+    love.mintmousse.notify({ title = "MintMousse", text = "Noice" })
     timer = 0
   end
 end
@@ -67,4 +80,8 @@ if love.window then
   love.draw = function()
     love.graphics.print(love.mouse.isDown(1) and "Mouse is down" or "Mouse is up")
   end
+end
+
+love.quit = function()
+  love.mintmousse.stop()
 end
